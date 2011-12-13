b569420eae4ea33419a51104d1041139ea8bd608
Jim V <jfv@stowers.org>
2011-12-13 11:28:38 -0600

    change generator to reportGenerator. add task

--- lib/hastie/generator.rb
+++ lib/hastie/generator.rb
@@ -1,4 +1,19 @@
-require 'thor'
+require 'thor/group'
 
-class Hastie::Generator < Thor::Group
+module Hastie
+  class ReportGenerator < Thor::Group
+    include Thor::Actions
+
+    desc "Creates framework for new report"
+    argument :name, :type => :string, :desc => "The name of the new report"
+    class_option :report_type, :default => :markdown
+
+    def self.source_root
+      File.dirname(__FILE__)
+    end
+
+    def create_test_file
+      create_file "#{name}/#{name}_report.markdown"
+    end
+  end
 end
