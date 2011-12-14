305cf311fb4f52d478f1da91b8f1ae66d66753d9
Jim V <jfv@stowers.org>
2011-12-14 15:04:31 -0600

    changed publish to server

--- lib/hastie/generator.rb
+++ lib/hastie/generator.rb
@@ -1,21 +1,21 @@
-require 'thor/group'
 require 'yaml'
-require 'hastie/config_file'
 require 'fileutils'
+require 'thor/group'
+require 'hastie/config_file'
 
 module Hastie
   class ReportGenerator < Thor::Group
     include Thor::Actions
+
     attr_accessor :report_id, :title, :analyst, :researcher, :pi
 
     CONFIG_FILE = File.expand_path(File.join("~", ".hastie"))
+    SERVER_REPORTS_FILE = "_reports.yml"
+    SERVER_CONFIG_FILE = "_config.yml"
 
     DATA_ROOT = "data"
     IMGS_ROOT = "imgs"
     REPORT_CONFIG_FILE = "report.yml"
-
-    SERVER_REPORTS_FILE = "_reports.yml"
-    SERVER_CONFIG_FILE = "_config.yml"
 
     desc "Creates framework for new report"
     argument :name, :type => :string, :desc => "The name of the new report. no spaces"
@@ -28,7 +28,8 @@ module Hastie
       File.dirname(__FILE__)
     end
 
-
+    # Tries to access users config file
+    # loads contents into the options hash
     def read_config_file
       if !File.exists? config_file
         say "No config file found. Please create #{config_file}"
@@ -38,43 +39,44 @@ module Hastie
       self.options = config.merge(self.options)
     end
 
-    def get_publish_config
-      # First check if the publishing directory exists
-      if !File.directory? options[:publish_root]
-        say "Cannot find publishing directory:"
-        say options[:publish_root]
-        say "Please modify \'publish_root\' to point to publishing root directory"
+    # Tries to access the servers 
+    def get_server_config
+      # First check if the server directory exists
+      if !File.directory? options[:server_root]
+        say "Cannot find server directory:"
+        say options[:server_root]
+        say "Please modify \'server_root\' to point to server root directory"
         exit(1)
       end
 
-      # Check for config file inside publish_root
-      publish_config_file = File.join(options[:publish_root], SERVER_CONFIG_FILE)
-      if !File.exists? publish_config_file
-        say "Cannot find config file in publishing directory:"
-        say publish_config_file
+      # Check for config file inside server_root
+      server_config_file = File.join(options[:server_root], SERVER_CONFIG_FILE)
+      if !File.exists? server_config_file
+        say "Cannot find config file in server directory:"
+        say server_config_file
         say ""
-        say "Are you sure #{options[:publish_root]} is a valid publish directory?"
+        say "Are you sure #{options[:server_root]} is a valid server directory?"
         exit(1)
       end
 
-      # Check for reports file inside publish_root
-      publish_report_file = File.join(options[:publish_root], SERVER_REPORTS_FILE)
-      if !File.exists? publish_report_file
-        say "Cannot find report file in publishing directory:"
-        say publish_report_file
+      # Check for reports file inside server_root
+      server_report_file = File.join(options[:server_root], SERVER_REPORTS_FILE)
+      if !File.exists? server_report_file
+        say "Cannot find report file in server directory:"
+        say server_report_file
         say ""
-        say "Are you sure #{options[:publish_root]} is a valid publish directory?"
+        say "Are you sure #{options[:server_root]} is a valid server directory?"
         exit(1)
       end
 
-      # merge the publish config and report file
+      # merge the server config and report file
       # put them in their own 'namespace' to avoid
       # collisions with existing configs
-      publish_config = ConfigFile.load(publish_config_file, :server)
-      self.options = publish_config.merge(self.options)
+      server_config = ConfigFile.load(server_config_file, :server)
+      self.options = server_config.merge(self.options)
 
-      publish_reports = ConfigFile.load(publish_report_file, :reports)
-      self.options = publish_reports.merge(self.options)
+      server_reports = ConfigFile.load(server_report_file, :reports)
+      self.options = server_reports.merge(self.options)
     end
 
     def setup_variables
@@ -85,6 +87,7 @@ module Hastie
       # out to be too loose to use
       self.report_id = name
       options[:report_id] = self.report_id
+      say_status "create", "report directory: #{report_id}"
       self.analyst = options[:analyst] || "unknown"
       self.researcher = options[:researcher] || "unknown"
       self.pi = options[:pi] || "unknown"
@@ -115,7 +118,7 @@ module Hastie
     def fetch_static_files
       options[:server]["static"] ||= []
       options[:server]["static"].each do |static_file|
-        static_path = File.join(options[:publish_root], static_file)
+        static_path = File.join(options[:server_root], static_file)
         destination_root = File.join(self.destination_root, report_id)
         if File.exists? static_path
           say_status "copy", "#{static_path} to #{File.basename(destination_root)}"
