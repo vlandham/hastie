437b10487ad61e53396c11abe799f2f818320820
Jim V <jfv@stowers.org>
2011-12-14 10:30:19 -0600

    reading data from users config file

--- lib/hastie/generator.rb
+++ lib/hastie/generator.rb
@@ -1,32 +1,60 @@
 require 'thor/group'
+require 'yaml'
+require 'hastie/hash_extras'
 
 module Hastie
   class ReportGenerator < Thor::Group
     include Thor::Actions
     attr_accessor :title, :analyst, :researcher, :pi
 
+    CONFIG_FILE = File.expand_path(File.join("~", ".hastie"))
+
     DATA_ROOT = "data"
     IMGS_ROOT = "imgs"
+    PUBLISH_ROOT_CONFIG = :publish_root
 
     desc "Creates framework for new report"
     argument :name, :type => :string, :desc => "The name of the new report. no spaces"
-    class_option :type, :aliases => "-t", :default => :markdown, :desc => "Type of report to generate"
-    class_option :analyst, :aliases => "-a", :default => "jfv", :desc => "Analyst generating the report"
-    class_option :researcher, :aliases => "-r", :default => "uknown", :desc => "Researcher the report is for"
-    class_option :pi, :aliases => "-p", :default => "uknown", :desc => "PI the researcher is under"
+    class_option :type, :aliases => "-t", :desc => "Type of report to generate"
+    class_option :analyst, :aliases => "-a", :desc => "Analyst generating the report"
+    class_option :researcher, :aliases => "-r", :desc => "Researcher the report is for"
+    class_option :pi, :aliases => "-p", :desc => "PI the researcher is under"
 
     def self.source_root
       File.dirname(__FILE__)
     end
 
-    def setup
-      self.title = name.gsub("_", " ").capitalize
-      self.analyst = options[:analyst]
-      self.researcher = options[:researcher]
-      self.pi = options[:pi]
+
+    def read_config_file
+      if !File.exists? config_file
+        say "No config file found. Please create #{config_file}"
+        exit(1)
+      end
+      config = YAML.load(File.read(config_file))
+      puts options.class
+      if config
+        say "loading config file"
+        config = Thor::CoreExt::HashWithIndifferentAccess.new(config)
+        self.options = config.merge(self.options)
+      end
     end
 
+    def setup_variables
+      self.title = name.gsub("_", " ").capitalize
+      self.analyst = options[:analyst] || "unknown"
+      self.researcher = options[:researcher] || "unknown"
+      self.pi = options[:pi] || "unknown"
+    end
+
+
     def check_name_availible
+      if !File.directory? options[:publish_root]
+        say "Cannot find publishing directory:"
+        say options[:publish_root]
+        say "Please modify this path to point to publishing root directory"
+        exit(1)
+      end
+
     end
 
     def create_report_file
@@ -68,6 +96,10 @@ module Hastie
 
       def imgs_dir
         File.join(IMGS_ROOT, name)
+      end
+
+      def config_file
+        CONFIG_FILE
       end
     end
   end
