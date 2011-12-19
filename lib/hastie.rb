e5037b969de7774ed160267a60381080e623bf9a
Jim V <jfv@stowers.org>
2011-12-19 09:40:31 -0600

    add rakefile

---
+++ lib/hastie.rb
@@ -0,0 +1,4 @@
+
+require 'hastie/report_generator'
+require 'hastie/report_publisher'
+require 'hastie/server_generator'
--- lib/hastie/version.rb
+++ lib/hastie/version.rb
@@ -1,4 +1,3 @@
 module Hastie
-  VERSION = "0.0.2".freeze
+  VERSION = "0.0.1"
 end
-
