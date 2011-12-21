2c909dd328a9cbe8a1a788fa61de8e8f8d334bdd
Jim V <jfv@stowers.org>
2011-12-21 16:45:53 -0600

    add server_config file constant

--- lib/hastie/constants.rb
+++ lib/hastie/constants.rb
@@ -2,6 +2,7 @@ module Hastie
   CONFIG_FILE = File.expand_path(File.join("~", ".hastie"))
   SERVER_REPORTS_FILE = "_reports.yml"
   SERVER_CONFIG_FILE = "_config.yml"
+  SERVER_PUBLISH_CONFIG_FILE = "_server_config.yml"
 
   DATA_ROOT = "data"
   IMGS_ROOT = "imgs"
