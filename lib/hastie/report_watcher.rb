eb0d13bada279b84f49bf20efde67a5add0b408c
Jim V <jfv@stowers.org>
2012-01-26 09:59:44 -0600

    now opens url if possible

--- lib/hastie/report_watcher.rb
+++ lib/hastie/report_watcher.rb
@@ -5,6 +5,8 @@ require 'thor/group'
 
 module Hastie
   class ReportWatcher < Thor::Group
+    include Thor::Actions
+
     desc "Watches Directory Using Jekyll"
     argument :path, :type => :string, :default => ".", :desc => "The path to the report directory"
 
@@ -12,30 +14,48 @@ module Hastie
 
     # this is done just as a double check that we are in a report directory
     def check_report_file
-      self.report_dir = File.expand_path(name)
+      self.report_dir = File.expand_path(path)
       report_config_file = File.join(report_dir, Hastie.report_config_name)
       if !File.exists? report_config_file
         say_status "error","Cannot locate #{Hastie.report_config_name}.", :red
-        say_status "error","Current directory is not a report.", :red
+        say_status "error","Directory #{self.report_dir} is not a report.", :red
         exit(1)
       end
+    end
+
+    def set_root
+      self.destination_root = self.report_dir
     end
 
     def read_config_file
       config_file = File.join(self.report_dir, Hastie.watch_config_file)
       if !File.exists? config_file
         say_status "error", "Cannot find #{config_file}", :red
-        say_status "error","Current directory is not a report.", :red
+        say_status "error","Directory #{self.report_dir} is not a report.", :red
         exit(1)
       end
-      local_config = ConfigFile.load(report_config_file, :local)
+      local_config = ConfigFile.load(config_file, :local)
       self.options = local_config.merge(self.options)
     end
 
     def start_jekyll
-      puts self.options
-        # puts "Starting Jekyll..."
-        # system("jekyll --auto --server")
+      port = self.options["local"]["server_port"] || "4000"
+      url = "http://0.0.0.0:#{port}"
+      say_status "open", url
+
+      pid = fork do
+        begin
+          require 'launchy'
+          sleep 4
+          Launchy.open(url)
+        rescue LoadError
+        rescue
+        end
+      end
+
+      in_root do
+        exec("jekyll --auto --server")
+      end
     end
   end
 end
---
+++ spec/report_watcher_spec.rb
@@ -0,0 +1,77 @@
+require 'fileutils'
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/fakefs_helper')
+require 'hastie/report_watcher'
+
+describe Hastie::ReportWatcher do
+
+  def read_file file
+    if File.exists? file
+      File.open(file, 'r').read
+    else
+      ""
+    end
+  end
+
+  before :each do
+    @config_file = File.expand_path(File.join(File.dirname(__FILE__), "fixtures", "hastie_config"))
+    @org_server_dir = File.expand_path(File.join(File.dirname(__FILE__), "fixtures", "server"))
+    @org_report_dir = File.expand_path(File.join(File.dirname(__FILE__), "fixtures", "report"))
+
+    @sandbox = File.expand_path(File.join(File.dirname(__FILE__), "sandbox"))
+
+    @report_dir = File.join(@sandbox, "report")
+
+    FileUtils.mkdir_p @sandbox
+
+    FileUtils.cp_r @org_report_dir, @report_dir
+
+    @report_name = File.basename(Dir.glob(File.join(@report_dir, "*.textile"))[0])
+
+    @input = [@report_dir]
+
+  end
+
+  after :each do
+    FileUtils.rm_r @sandbox if File.exists?(@sandbox)
+  end
+
+  it "should not error if in a report dir" do
+    content = ""
+    pid = fork do
+      content = capture(:stdout) do
+        lambda { Hastie::ReportWatcher.start @input }.should_not raise_error SystemExit
+      end
+    end
+
+    sleep 5
+
+    Process.kill 'INT', pid
+    Process.wait pid
+  end
+
+  it "should raise errror if not in a report dir" do
+    input = [@sandbox]
+    content = capture(:stdout) do
+      lambda { Hastie::ReportWatcher.start input }.should raise_error SystemExit
+    end
+  end
+
+end
+
+describe Hastie::ReportWatcher, fakefs: true do
+  before :each do
+    # tested in server_reader_spec.rb
+    FakeFsHelper.stub_config_file
+    FakeFsHelper.stub_reports_file
+    FakeFsHelper.stub_server_config
+
+    @report_dir = "/tmp/test_report"
+    FileUtils.mkdir_p @report_dir
+    File.exists?(@report_dir).should == true
+  end
+
+  it "should be happy" do
+
+  end
+end
